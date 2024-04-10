6ddd2a6ebc4b76d7c0a294eb518befa5ba91e785
Vincent Pantia <vincent.m.pantia@hotmail.com>
2024-04-10 19:09:13 +0800

    Minor: Add Base Repository

---
+++ RWM/RWM.Domain/Contractors/Repositories/IBaseRepository.cs
@@ -0,0 +1,12 @@
+﻿using System.Linq.Expressions;
+
+namespace RWM.Domain.Contractors.Repositories
+{
+    public interface IBaseRepository<TEntity>
+    {
+        Task<IQueryable<TEntity>> GetByExpressionAsync(Expression<Func<TEntity, bool>> expression);
+        Task<TEntity?> GetByIdAsync<TId>(TId id);
+        Task AddAsync(TEntity entity);
+        Task UpdateAsync(TEntity entity);
+    }
+}
---
+++ RWM/RWM.Infrastructure/Databases/Repositories/BaseRepository.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RWM.Domain.Contractors.Repositories;
+using RWM.Infrastructure.Databases.Contexts;
+using System.Linq.Expressions;
+
+namespace RWM.Infrastructure.Databases.Repositories
+{
+    public class BaseRepository<TEntity> : IBaseRepository<TEntity>
+        where TEntity : class
+    {
+        protected RWMDbContext _context;
+        protected DbSet<TEntity> _table;
+        public BaseRepository(RWMDbContext context)
+        {
+            _context = context;
+            _table = context.Set<TEntity>();
+        }
+
+        public async Task<IQueryable<TEntity>> GetByExpressionAsync(Expression<Func<TEntity, bool>> expression) =>
+            await Task.Run(() => _table.Where(expression)
+                                       .AsNoTracking());
+
+        public async Task<TEntity?> GetByIdAsync<TId>(TId id) =>
+            await _table.FindAsync(id);
+
+        public async Task AddAsync(TEntity entity) =>
+            await _table.AddAsync(entity);
+
+        public async Task UpdateAsync(TEntity entity) =>
+            await Task.Run(() => _table.Update(entity));
+    }
+}
